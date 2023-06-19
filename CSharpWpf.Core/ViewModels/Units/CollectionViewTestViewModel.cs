620884f3ba49dd0207b59453bb6dd637a6056f85
pnehanol <pnehanol@wonikpne.com>
2023-06-19 20:42:56 +0900

    CollectionViewTest 1일차< 1. Mvvm구성 2. 구현진행중 >

---
+++ CSharpWpf.Core/ViewModels/Units/CollectionViewTestViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpWpf.Core.Models;
+
+namespace CSharpWpf.Core.ViewModels.Units
+{
+    public partial class CollectionViewTestViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<Person> people;
+    }
+}
